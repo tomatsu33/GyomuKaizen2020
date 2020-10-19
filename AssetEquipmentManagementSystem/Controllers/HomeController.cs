328c5c3abf370891265683c8468bb41a6a662f1b
tomatsu33 <tmatsushita@j-skk.com>
2020-10-19 14:01:44 +0900

    add all

---
+++ AssetEquipmentManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AssetEquipmentManagementSystem.Models;
+
+namespace AssetEquipmentManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddItem()
+        {
+            return View();
+        }
+
+        public IActionResult SearchItem()
+        {
+            return View();
+        }
+
+        public IActionResult PrintList()
+        {
+            return View();
+        }
+
+        public IActionResult GetCheckPoint()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
